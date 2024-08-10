96a7a03b4cca138044897c35f8ae20c7a34b67b2
denzio231 <108673932+denzio231@users.noreply.github.com>
2024-08-10 22:18:13 +0800

    Add files via upload

--- Evaluator.hpp
+++ Evaluator.hpp
@@ -3,6 +3,20 @@
 #include <string>
 #include <sstream>
 #include <limits>
+#include <functional>
+#include <sstream>
+#include <cctype>
+#include <stdexcept>
+#include<map>
+#include<cmath>
+std::vector<std::string> op_priority = { "+-", "*/", "^" };
+std::map<char, std::function<double(double, double)>> op_func = {
+{'+', [](double x, double y) { return x + y; }},
+{'-', [](double x, double y) { return x - y; }},
+{'*', [](double x, double y) { return x * y; }},
+{'/', [](double x, double y) { return x / y; }},
+{'^', [](double x, double y) { return pow(x, y); }}
+};
 std::string removeBrackets(std::string evalStr) {
     int c = 0;
     bool flag = false;
@@ -39,7 +53,7 @@ std::string removeBrackets(std::string evalStr) {
     return ret;
 }
 
-std::tuple<std::vector<std::string>, std::vector<char>, bool> splitBy(const std::string& operators, std::string evalStr) {
+std::tuple<std::vector<std::string>, std::vector<char>, bool> splitBy(const std::string& operators, std::string evalStr,int p) {
     std::vector<std::string> splitList;
     std::vector<char> ops;
     std::string buff = "";
@@ -58,7 +72,7 @@ std::tuple<std::vector<std::string>, std::vector<char>, bool> splitBy(const std:
 
     for (size_t idx = 0; idx < evalStr.size(); idx++) {
         if (evalStr[idx] == '(') {
-            flag = true;        
+            flag=false;
             openBracket = true;
             buff += evalStr[idx];
             continue;
@@ -90,45 +104,29 @@ std::tuple<std::vector<std::string>, std::vector<char>, bool> splitBy(const std:
     splitList.push_back(buff);
     return {splitList, ops, noSplitFlag};
 }
-
 double eval(std::string evalStr,double x) {
-    auto [splitList, ops, noSplitFlag] = splitBy("+-", evalStr);
-    if (noSplitFlag) {
-        auto [splitList2, ops2, noMultFlag] = splitBy("*/", evalStr);
 
-        if (noMultFlag) {
-            if(evalStr=="x"){
-                return x;
-            }
-            try {
-                return std::stod(evalStr);
-            } catch (const std::exception& e) {
-                return std::numeric_limits<double>::quiet_NaN();
-            }
+    for (size_t p = 0; p < op_priority.size(); p++) {
+        auto [splitList, ops, noSplitFlag] = splitBy(op_priority[p], evalStr,p);
+        if (noSplitFlag) {
+            continue;
         } else {
-            double val = eval(splitList2[0],x);
-            for (size_t idx = 0; idx < ops2.size(); idx++) {
-                char op = ops2[idx];
-                double curr = eval(splitList2[idx + 1],x);
-                if (op == '*') {
-                    val *= curr;
-                } else if (op == '/') {
-                    val /= curr;
-                }
+            double val = eval(splitList[0],x);
+            for (size_t idx = 0; idx < ops.size(); idx++) {
+                char op = ops[idx];
+                double curr = eval(splitList[idx + 1],x);
+                auto func = op_func[op];
+                val = func(val, curr);
             }
             return val;
         }
-    } else {
-        double sum = eval(splitList[0],x);
-        for (size_t idx = 0; idx < ops.size(); idx++) {
-            char op = ops[idx];
-            double val = eval(splitList[idx + 1],x);
-            if (op == '+') {
-                sum += val;
-            } else if (op == '-') {
-                sum -= val;
-            }
-        }
-        return sum;
+    }
+    if(evalStr=="x"){
+        return x;
+    }
+    try {
+        return std::stod(evalStr);
+    } catch (const std::exception& e) {
+        return std::numeric_limits<double>::quiet_NaN();
     }
 }
